c2402d9cead6046700e73aeda0e2800cce372413
SherryH <enjoy.wind7rain@gmail.com>
2019-03-16 12:31:49 +1300

    Made Carousel render when visible

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
+import OnVisible from 'react-on-visible';
 import './carousel.css';
 
 const Carousel = () => {
   const carouselRef = React.createRef();
+  const [isVisible, setIsVisible] = useState(false);
   useEffect(() => {
-    console.log('carouselRef', carouselRef.current);
     for (let i = 0; i < 200; i++) {
       const box = document.createElement('div');
       box.classList.add('box');
@@ -22,33 +23,37 @@ const Carousel = () => {
     speed: 2000,
     autoplaySpeed: 2000,
     cssEase: 'linear',
+    className: 'invisible',
   };
   return (
     <div ref={carouselRef}>
       <h2>Auto Play</h2>
-      <Slider {...settings}>
-        <div>
-          <img src="https://images.pexels.com/photos/1934259/pexels-photo-1934259.jpeg" alt="Italian Trulli" />
-        </div>
-        <div>
-          <img src="https://images.pexels.com/photos/290320/pexels-photo-290320.jpeg" alt="Italian Trulli" />
-        </div>
-        <div>
-          <img src="https://images.pexels.com/photos/1309778/pexels-photo-1309778.jpeg" alt="Italian Trulli" />
-        </div>
-        <div>
-          <img src="https://images.pexels.com/photos/631160/pexels-photo-631160.jpeg" alt="Italian Trulli" />
-          <h3>4</h3>
-        </div>
-        <div>
-          <img src="https://images.pexels.com/photos/631160/pexels-photo-631160.jpeg" alt="Italian Trulli" />
-          <h3>5</h3>
-        </div>
-        <div>
-          <img src="https://images.pexels.com/photos/631160/pexels-photo-631160.jpeg" alt="Italian Trulli" />
-          <h3>6</h3>
-        </div>
-      </Slider>
+      <OnVisible onChange={(visible) => { console.log('visible', visible); setIsVisible(visible); console.log('isVisible', isVisible); }}>
+
+        <Slider {...settings}>
+          <div>
+            <img src="https://images.pexels.com/photos/1934259/pexels-photo-1934259.jpeg" alt="Italian Trulli" />
+          </div>
+          <div>
+            <img src="https://images.pexels.com/photos/290320/pexels-photo-290320.jpeg" alt="Italian Trulli" />
+          </div>
+          <div>
+            <img src="https://images.pexels.com/photos/1309778/pexels-photo-1309778.jpeg" alt="Italian Trulli" />
+          </div>
+          <div>
+            <img src="https://images.pexels.com/photos/631160/pexels-photo-631160.jpeg" alt="Italian Trulli" />
+            <h3>4</h3>
+          </div>
+          <div>
+            <img src="https://images.pexels.com/photos/631160/pexels-photo-631160.jpeg" alt="Italian Trulli" />
+            <h3>5</h3>
+          </div>
+          <div>
+            <img src="https://images.pexels.com/photos/631160/pexels-photo-631160.jpeg" alt="Italian Trulli" />
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </OnVisible>
     </div>
   );
 };
